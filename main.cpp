13d595c9a6899e77777ceead61a004cb561b21fe
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-01-22 20:02:49 -0500

    Structure of a program

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@
 //  Copyright © 2017 Marcus Karavan. All rights reserved.
 //
 
-#include <iostream>
+// Chapters covered so far:
+// http://www.cplusplus.com/doc/tutorial/program_structure/
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+using namespace std;    					 // <- Need this to call cout with its unqualified name
+
+int main(int argc, const char * argv[]) {    // <- void main() doesn't seem to work, only int main()
+	// whitespace doesn't matter
+    std::cout << "Hello, World!\n";          // <- STandard Character OUTput device
     std::cout << "Hello, World Again!\n";
-    return 0;
+
+    std::cout << "I can explicitly call cout with its qualified namespace from std...\n";
+    cout << "...or I can call it with it's unqualified name as long as I have a 'using declaration'";
+    //return 0; <- I've disabled return, but it compiles and returns 0.  Why?
 }
