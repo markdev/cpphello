fc91da97ba80f20b2ce9a78da73f44ad0fb8d6d4
Marcus Karavan <karavanm@Marcuss-MacBook-Pro.local>
2017-01-22 19:16:12 -0500

    Creates hello world in c++

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Marcus Karavan on 1/22/17.
+//  Copyright © 2017 Marcus Karavan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World Again!\n";
+    return 0;
+}
